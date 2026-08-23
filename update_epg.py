43de5f691f7d7945d9cc463fd6937d728983f3a2
senadhtc1-jpg <senadhtc1@gmail.com>
2026-08-23 08:56:45 +0200

    Update update_epg.py

--- update_epg.py
+++ update_epg.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
+import copy
 import gzip
-import io
 import urllib.request
 import xml.etree.ElementTree as ET
 from pathlib import Path
@@ -46,6 +46,12 @@ KEEP_IDS = {
     'Televizija.Hema.ba',
 }
 
+# EPGShare koristi stari ID za Nova BH.
+# U našem SenoEPG-u ga pretvaramo u 108.ba
+ID_REMAP = {
+    'Nova.BH.HD.(BIH).ba': '108.ba',
+}
+
 OUT_XML = Path("senoepg-bih.xml")
 OUT_GZ = Path("senoepg-bih.xml.gz")
 
@@ -71,14 +77,30 @@ new_root = ET.Element(
 channel_count = 0
 programme_count = 0
 
+# Kanali
 for channel in source_root.findall("channel"):
-    if channel.attrib.get("id") in KEEP_IDS:
-        new_root.append(channel)
+    source_id = channel.attrib.get("id")
+
+    if source_id in KEEP_IDS:
+        new_channel = copy.deepcopy(channel)
+
+        # Nova BH: stari ID -> 108.ba
+        new_channel.attrib["id"] = ID_REMAP.get(source_id, source_id)
+
+        new_root.append(new_channel)
         channel_count += 1
 
+# Programski raspored
 for programme in source_root.findall("programme"):
-    if programme.attrib.get("channel") in KEEP_IDS:
-        new_root.append(programme)
+    source_id = programme.attrib.get("channel")
+
+    if source_id in KEEP_IDS:
+        new_programme = copy.deepcopy(programme)
+
+        # Nova BH program također mora koristiti 108.ba
+        new_programme.attrib["channel"] = ID_REMAP.get(source_id, source_id)
+
+        new_root.append(new_programme)
         programme_count += 1
 
 ET.indent(new_root, space="  ")
@@ -90,3 +112,4 @@ with OUT_XML.open("rb") as src, gzip.open(OUT_GZ, "wb", compresslevel=9) as dst:
 
 print(f"Updated {OUT_XML}: {channel_count} channels, {programme_count} programme entries")
 print(f"Updated {OUT_GZ}")
+print("Nova BH EPG ID: 108.ba")
